232b72e412d71d40fef51f1dce30c7fdc140bcdd
Magomed-Workout <128885397+Magomed-Workout@users.noreply.github.com>
2024-02-01 21:08:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function onscroll(entry) {
 let options = {
     threshold: [0.5] };
 let observer = new IntersectionObserver(onscroll, options);
-let elements = document.querySelectorAll('.wrap, .header, .title, .subtitle, .description, .description-subtitle, .description-main, .video, .footer, .container, .container1, .show1, .show2, .show3, .show4, .show5, .show6, .map');
+let elements = document.querySelectorAll('.wrap, .header, .title, .subtitle, .description, .description-subtitle, .description-main, .description-monument, .monument, .video, .footer, .container, .container1, .show1, .show2, .show3, .show4, .show5, .show6, .show7, .map');
 
 for (let elm of elements) {
     observer.observe(elm);
